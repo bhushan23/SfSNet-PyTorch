aac6fd41e008d166ce965c73054b8c559adb6bcc
bhushan <bhushan.s.94@gmail.com>
2019-03-23 16:33:30 -0400

    dataset - generate csv of images and read from it

--- data_loading.py
+++ data_loading.py
@@ -12,7 +12,53 @@ import pandas as pd
 
 IMAGE_SIZE = 128
 
-def get_dataset(dir, validation_split=0):
+def generate_data_csv(dir, save_location):
+    albedo = set()
+    normal = set()
+    depth  = set()
+    mask   = set()
+    face   = set()
+    sh     = set()
+
+    name_to_set = {'albedo' : albedo, 'normal' : normal, 'depth' : depth, \
+                    'mask' : mask, 'face' : face, 'light' : sh}
+    
+    for k, v in name_to_set.items():
+        regex_str = '*/*_' + k + '_*'
+        for img in sorted(glob.glob(dir + regex_str)):
+            timg = img.split('/')
+            folder_id = timg[-2]
+            name      = timg[-1].split('.')[0]
+            name      = name.split('_')
+            assert(len(name) == 4)
+            name      = folder_id + '_' + name[0] + '_' + name[2] + '_' + name[3]
+            v.add(name)
+
+    final_images = set.intersection(albedo, normal, depth, mask, face, sh)    
+
+    albedo = []
+    normal = []
+    depth  = []
+    mask   = []
+    face   = []
+    sh     = []
+
+    name_to_list = {'albedo' : albedo, 'normal' : normal, 'depth' : depth, \
+                    'mask' : mask, 'face' : face, 'light' : sh}
+
+    for img in final_images:
+        split = img.split('_')
+        for k, v in name_to_list.items():
+            ext = '.png'
+            if k == 'light':
+                ext = '.txt'
+            file_name = split[0] + '/' + k + '_' + '_'.join(split[1:]) + ext
+            v.append(file_name)
+
+    df = pd.DataFrame(data=name_to_list)
+    df.to_csv(save_location)
+
+def get_dataset(dir, read_from_csv=None, validation_split=0):
     albedo  = []
     sh      = []
     mask    = []
@@ -20,41 +66,39 @@ def get_dataset(dir, validation_split=0):
     face    = []
     depth   = []
 
-    for img in sorted(glob.glob(dir + '*/*_albedo_*')):
-        albedo.append(img)
+    if read_from_csv is None:
+        for img in sorted(glob.glob(dir + '*/*_albedo_*')):
+            albedo.append(img)
 
-    for img in sorted(glob.glob(dir + '*/*_face_*')):
-        face.append(img)    
+        for img in sorted(glob.glob(dir + '*/*_face_*')):
+            face.append(img)    
 
-    for img in sorted(glob.glob(dir + '*/*_normal_*')):
-        normal.append(img)
+        for img in sorted(glob.glob(dir + '*/*_normal_*')):
+            normal.append(img)
 
-    for img in sorted(glob.glob(dir + '*/*_depth_*')):
-        depth.append(img)
+        for img in sorted(glob.glob(dir + '*/*_depth_*')):
+            depth.append(img)
 
-    for img in sorted(glob.glob(dir + '*/*_mask_*')):
-        mask.append(img)
+        for img in sorted(glob.glob(dir + '*/*_mask_*')):
+            mask.append(img)
 
-    for img in sorted(glob.glob(dir + '*/*_light_*.txt')):
-        sh.append(img)
+        for img in sorted(glob.glob(dir + '*/*_light_*.txt')):
+            sh.append(img)
+    else:
+        df = pd.read_csv(read_from_csv)
+        albedo = list(df['albedo'])
+        face   = list(df['face'])
+        normal = list(df['normal'])
+        depth  = list(df['depth'])
+        mask   = list(df['mask'])
+        sh     = list(df['light'])
 
-    # Debugging print out images considered
-    # with open('albedo.txt', 'w') as f:
-    #     for item in albedo:
-    #         f.write("%s\n" % item)    
+        name_to_list = {'albedo' : albedo, 'normal' : normal, 'depth' : depth, \
+                    'mask' : mask, 'face' : face, 'light' : sh}
 
-    # with open('face.txt', 'w') as f:
-    #     for item in face:
-    #         f.write("%s\n" % item)    
+        for _, v in name_to_list.items():
+            v[:] = [dir + el for el in v]
 
-    # with open('normal.txt', 'w') as f:
-    #     for item in normal:
-    #         f.write("%s\n" % item)    
-
-    # with open('mask.txt', 'w') as f:
-    #     for item in mask:
-    #         f.write("%s\n" % item)    
-    print(len(albedo) , len(face) , len(normal) , len(depth) , len(mask) , len(sh))
     assert(len(albedo) == len(face) == len(normal) == len(depth) == len(mask) == len(sh))
     dataset_size = len(albedo)
     validation_count = int (validation_split * dataset_size / 100)
@@ -72,6 +116,9 @@ def get_dataset(dir, validation_split=0):
     # Shall we just split manually to ensure run-to-run train-val dataset is same?
     train_dataset, val_dataset = random_split(full_dataset, [train_count, validation_count])
     return train_dataset, val_dataset
+
+# generate_data_csv('./data/train/', './data/train.csv')
+# generate_data_csv('./data/test/', './data/test.csv')
 
 class SfSNetDataset(Dataset):
     def __init__(self, albedo, face, normal, mask, sh, transform = None):
@@ -94,7 +141,6 @@ class SfSNetDataset(Dataset):
         mask   = self.mask_transform(Image.open(self.mask[index]))
         pd_sh  = pd.read_csv(self.sh[index], sep='\t', header = None)
         sh     = torch.tensor(pd_sh.values).type(torch.float)
-
         return albedo, normal, mask, sh, face
 
     def __len__(self):
