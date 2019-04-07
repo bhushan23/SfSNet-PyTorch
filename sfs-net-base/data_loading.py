Fix Normal Range and remove normalization

sfs-net-base/data_loading.py
@@ -174,8 +174,7 @@ def get_sfsnet_dataset(syn_dir=None, read_from_csv=None, read_celeba_csv=None, r
     # Build custom datasets
     transform = transforms.Compose([
                 transforms.Resize(IMAGE_SIZE),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
+                transforms.ToTensor()
             ])
     
     full_dataset = SfSNetDataset(albedo, face, normal, mask, sh, transform)  
@@ -202,9 +201,8 @@ def get_celeba_dataset(dir=None, read_from_csv=None, read_first=None, validation
     # Build custom datasets
     transform = transforms.Compose([
                 transforms.Resize(IMAGE_SIZE),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
-            ])
+                transforms.ToTensor()
+                ])
     
     full_dataset = CelebADataset(face, transform)  
     # TODO: This will vary dataset run-to-run
@@ -274,11 +272,18 @@ class SfSNetDataset(Dataset):
                               transforms.Resize(IMAGE_SIZE),
                               transforms.ToTensor(),
                             ])
+        self.normal_transform = transforms.Compose([
+                              transforms.Resize(IMAGE_SIZE)
+                            ])
 
     def __getitem__(self, index):
         albedo = self.transform(Image.open(self.albedo[index]))
         face   = self.transform(Image.open(self.face[index]))
-        normal = self.transform(Image.open(self.normal[index]))
+        # normal = io.imread(self.face[index]))
+        normal = self.normal_transform(Image.open(self.normal[index]))
+        normal = torch.tensor(np.asarray(normal)).permute([2, 0, 1])
+        normal = normal.type(torch.float)
+        normal = (normal - 128) / 128
         if self.mask[index] == 'None':
             # Load dummy 1 mask for CelebA
             # To ensure consistency if mask is used
@@ -299,7 +304,7 @@ class CelebADataset(Dataset):
         self.dataset_len = len(self.face)
         self.mask_transform = transforms.Compose([
                               transforms.Resize(IMAGE_SIZE),
-                              transforms.ToTensor(),
+                              transforms.ToTensor()
                             ])
 
     def __getitem__(self, index):

sfs-net-base/shading.py
@@ -3,6 +3,8 @@ import torchvision
 import numpy as np
 import torch.nn as nn
 from torch.autograd import Variable
+from torchvision import transforms
+import pandas as pd
 
 from utils import *
 from models import sfsNetShading
@@ -105,15 +107,9 @@ def getShadingFromNormalAndSH(Normal, rSH):
     return outShadingB
 
 
-def validate_shading_method(train_dl, wandb):
+def validate_shading_method(train_dl):
     albedo, normal, mask, sh, face = next(iter(train_dl))
 
-    face_ndarr = get_image_grid(face, mask=mask)
-    wimg = wandb.Image(face_ndarr, caption="Ground Truth Face")
-    wandb.log({'Face Image': wimg})
-
-    normal = denorm(normal)
-    albedo = denorm(albedo)
     shading = getShadingFromNormalAndSH(normal, sh)
     save_image(albedo,  denormalize=False, mask=mask, path='./results/shading_from_normal/albedo.png')
     save_image(normal,  denormalize=False, mask=mask, path='./results/shading_from_normal/normal.png')